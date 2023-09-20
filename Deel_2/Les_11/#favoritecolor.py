657b21eb8d81e7a0cb1815f030525f21fe569283
mojtaba <mojtaba.alavi41@gmail.com>
2023-09-20 13:50:20 +0200

    opdrachten zijn klaar

---
+++ Deel_2/Les_11/#favoritecolor.py
@@ -0,0 +1,11 @@
+#favoritecolor
+
+
+import random
+
+favoriteColor = input('En wat is je favoriete kleur? ') 
+trueOrFalse = random.randint(0,1)
+if trueOrFalse:
+    print('Ik vind dat ook een mooie kleur!')
+else:
+    print('TBH, ik hou niet zo van {}...'.format(favoriteColor))
